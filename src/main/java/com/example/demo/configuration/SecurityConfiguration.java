443adbc8659137858bc831422c30366d03c4bb04
Rajesh <rajeshkilladi96@gmail.com>
2024-08-19 11:50:41 +0530

    Security configuration changed

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import javax.sql.DataSource;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatchers;
 
 @Configuration
 @EnableWebSecurity
@@ -31,47 +30,48 @@ public class SecurityConfiguration {
 	public void authorityManager(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder())
-		 .usersByUsernameQuery("SELECT username, password,enabled FROM user_master WHERE username = ?")
-	        .authoritiesByUsernameQuery("SELECT username, role FROM user_master WHERE username = ?");
+				.usersByUsernameQuery("SELECT username, password,enabled FROM user_master WHERE username = ?")
+				.authoritiesByUsernameQuery("SELECT username, role FROM user_master WHERE username = ?");
 	}
 
 	@Bean
 	SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeHttpRequests((req) -> req
-	            .requestMatchers(HttpMethod.DELETE,"/user/{id}","/room/{id}").hasRole("ADMIN")
-	            .requestMatchers(HttpMethod.GET,"/users","/bookings").hasRole(ADMIN)
-	            .requestMatchers(HttpMethod.PUT,"/room").hasRole(ADMIN)
-	            .requestMatchers(HttpMethod.POST,"/room").hasRole(ADMIN)
-	            
-	            
-	            .requestMatchers(HttpMethod.PUT, "/user").hasAnyRole(ADMIN, USER)
-	            .requestMatchers(HttpMethod.GET,"/availablerooms","/rooms","/room/{id}","/booking/{id}").hasAnyRole(ADMIN, USER)
-	            .requestMatchers(HttpMethod.DELETE,"/booking/{id}").hasAnyRole(ADMIN, USER)
-	            .requestMatchers(HttpMethod.POST, "/booking").hasAnyRole(ADMIN, USER)
-	            .requestMatchers(HttpMethod.POST,"/user").permitAll()
-	            .requestMatchers(HttpMethod.GET,"user/{id}").permitAll()// Allow PUT to /user for ADMIN and USER
-	            
-	            .requestMatchers("/h2-console/**").permitAll()  // Allow access to H2 console
-	            .requestMatchers("/swagger-ui/**").permitAll()  // Allow access to Swagger UI
-	            .requestMatchers("/v3/api-docs/**").permitAll()
-	            .requestMatchers("/**").permitAll()  // Allow access to other endpoints
-	            .anyRequest().authenticated())  // Authenticate all other requests
+		http.authorizeHttpRequests((req) -> req.requestMatchers(HttpMethod.DELETE, "/user/{id}", "/room/{id}")
+				.hasRole("ADMIN").requestMatchers(HttpMethod.GET, "/users", "/bookings").hasRole(ADMIN)
+				.requestMatchers(HttpMethod.PUT, "/room").hasRole(ADMIN).requestMatchers(HttpMethod.POST, "/room")
+				.hasRole(ADMIN)
+
+				.requestMatchers(HttpMethod.PUT, "/user").hasAnyRole(ADMIN, USER)
+				.requestMatchers(HttpMethod.GET, "/availablerooms", "/rooms", "/room/{id}", "/booking/{id}")
+				.hasAnyRole(ADMIN, USER).requestMatchers(HttpMethod.DELETE, "/booking/{id}").hasAnyRole(ADMIN, USER)
+				.requestMatchers(HttpMethod.POST, "/booking").hasAnyRole(ADMIN, USER)
+				.requestMatchers(HttpMethod.POST, "/user").permitAll().requestMatchers(HttpMethod.GET, "user/{id}")
+				.permitAll()// Allow PUT to /user for ADMIN and USER
+
+				.requestMatchers("/h2-console/**").permitAll() // Allow access to H2 console
+				.requestMatchers("/swagger-ui/**").permitAll() // Allow access to Swagger UI
+				.requestMatchers("/v3/api-docs/**").permitAll().requestMatchers("/**").permitAll() // Allow access to
+																									// other endpoints
+				.anyRequest().authenticated()) // Authenticate all other requests
 //	        .csrf(csrf -> csrf
 //	            .ignoringRequestMatchers(new AntPathRequestMatcher("/user", HttpMethod.POST.name()),new AntPathRequestMatcher("/h2-console/**")))  // Disable CSRF for POST requests to /user
 //	        	
-	        .httpBasic(withDefaults())  // Enable HTTP Basic Authentication
-	        .formLogin(withDefaults());  // Enable form-based login
-	    http.csrf(csrf->csrf.disable());
-	    http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+				.httpBasic(withDefaults()) // Enable HTTP Basic Authentication
+				.formLogin(withDefaults()); // Enable form-based login
+		http.csrf(csrf -> csrf.disable());
+		http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
-	    return http.build();
+		return http.build();
 	}
-	
-	
-	 @Bean
-	 BCryptPasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder();
-	    }
+
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	GroupedOpenApi api() {
+		return GroupedOpenApi.builder().group("v1").pathsToMatch("/**").build();
+	}
 
 }
